c5618ea1134b7eab5d0056764f88fb193c5d607c
Filip Štolfa <phillipstolfa@gmail.com>
2022-09-22 18:00:41 +0200

    fix: use UTC time when sending data to DB

--- break-notify.sh
+++ break-notify.sh
@@ -5,12 +5,12 @@ if [[ "$1" == "non-initial" ]]; then
   # -t (timeout) does not seem to work on cinnamon as expected -- max timeout is 5 secons
   # if -u (urgency) is set above normal, the timeout is ignored completely and notification stays forever
   # notify-send -u critical -i media-playback-pause -a "Breaky" "Take a break" "Take a short break, stretch, relax your eyes by looking into the distance"
-  PROMPT_DATE=$(date "+%Y-%m-%d %H:%M:%S")
+  PROMPT_DATE=$(TZ='UTC' date "+%Y-%m-%d %H:%M:%S")
 
   # blocking call
-  RES=$(notify-send.py -u critical -i media-playback-pause -a "Breaky" "Take a break" "Take a short break, stretch, relax your eyes by looking into the distance" --action break_short:"Short Break" break_full:"Full Break" skip:Skip | head -1)
+  RES=$(notify-send.py -u critical -i media-playback-pause -a "Breaky" "Take a break" "Take a short break, stretch, relax your eyes by looking into the distance" --action break_full:"Full Break" break_short:"Short Break" skip:Skip | head -1)
 
-  CLICK_DATE=$(date "+%Y-%m-%d %H:%M:%S")
+  CLICK_DATE=$(TZ='UTC' date "+%Y-%m-%d %H:%M:%S")
 
   if [[ "$RES" == "closed" ]]; then
     RES="break_full"
